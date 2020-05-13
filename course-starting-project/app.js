de42049a3736f4c5740da463402b78ae5933793d
LMTrain <layconmm@msn.com>
2020-05-12 23:28:08 -0500

    Working on Object

--- course-starting-project/app.js
+++ course-starting-project/app.js
@@ -1,14 +1,5 @@
-function merge(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 25;
-var number2 = 25.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-var result = merge(number1, number2, printResult, resultPhrase);
+var person = {
+    mame: 'Laycon',
+    age: '45'
+};
+console.log(person);
--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,17 +1,5 @@
-function merge(n1: number, n2: number, showResult: boolean, phrase: string) {
-    
-    const result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result)
-    } else {
-
-        return result;
-    }
-}
-
-const number1 = 25;
-const number2 = 25.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-const result = merge(number1, number2, printResult, resultPhrase);
+const person = {
+    mame: 'Laycon',
+    age: '45'
+};
+console.log(person)
