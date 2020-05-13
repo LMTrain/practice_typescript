840f551db18a150cedb26ef68858b0c191e44408
LMTrain <layconmm@msn.com>
2020-05-13 00:31:19 -0500

    Working on Tuple

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,8 +1,17 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]; //Tuples
+} = {
     name: 'Laycon',
     age: 30,
-    hobbies: ["Drawing", 'Swimming']
+    hobbies: ["Drawing", 'Swimming'],
+    role: [2, 'author'] //Tuples
+
 };
+
+person.role.push('admin');
 
 let favoriteActivities: string[];
 favoriteActivities = ['Singing'];
