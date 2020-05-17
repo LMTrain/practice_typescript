520c313b6217054ad21d6842733570c4d31a1cb6
LM Train <layconmm@msn.com>
2020-05-16 20:20:05 -0500

    Tyoes Aliases and Object Types

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,20 +1,22 @@
+type Combinable = number | string; //union type
+type ConversionDescriptor = 'as-number' | 'as-text';
 
 function merge(
-    input1: number | string, 
-    input2: number | string,  //union type
-    resultConversion: 'as-number' | 'as-text'
+   input1: Combinable, 
+   input2: Combinable,
+   resultConversion: ConversionDescriptor
 )   {
-    
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }    
-    return result;
+   
+   let result;
+   if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+       result = +input1 + +input2;
+   } else {
+       result = input1.toString() + input2.toString();
+   }    
+   return result;
 
 
-    
+   
 }
 
 const combinedAges = merge(30,36, 'as-number');
