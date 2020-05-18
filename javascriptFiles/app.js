8e6efe53a576c03cf36a094897b1262ea981f4a9
LM Train <layconmm@msn.com>
2020-05-17 23:55:01 -0500

    Arrow Function

--- javascriptFiles/app.js
+++ javascriptFiles/app.js
@@ -1,11 +1,19 @@
-var userInput;
-var userName;
-userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string') {
-    userName = userInput;
+"use strict";
+var userName = 'Max';
+var age = 30;
+age = 29;
+console.log(userName + ", " + age);
+var add = function (a, b) {
+    return a + b;
+};
+console.log(add(112, 531));
+var addTwo = function (a, b) { return a + b; };
+console.log(addTwo(112, 531));
+var printOutput = function (output) {
+    console.log(output);
+};
+var button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', function (event) { return console.log("THIS IS EVENT: " + event); });
 }
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateError('An error Occured!', 500);
+printOutput(add(5, 2));
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,29 @@
-let userInput: unknown;
-let userName: string;
+const userName = 'Max';
 
-userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string') {
-    userName = userInput;
+let age = 30;
+
+age = 29;
+ 
+console.log(userName + ", " + age)
+
+const add = (a: number, b: number) => {
+    return a + b;
+};
+
+console.log(add(112, 531));
+
+const addTwo = (a: number, b: number) => a + b;
+
+console.log(addTwo(112, 531));
+
+const printOutput = (output: string | number) => {
+    console.log(output);
+
 }
 
-function generateError(message: string, code: number) {
-    throw { message: message, errorCode: code };
-}
+const button = document.querySelector('button');
 
-generateError('An error Occured!', 500);
+if (button) {
+    button.addEventListener('click', event => console.log("THIS IS EVENT: " + event));
+}
+printOutput(add(5, 2));
