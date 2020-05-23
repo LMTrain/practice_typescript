925d2a27c008a0418dac6f9af5d4f0314069485e
LM Train <layconmm@msn.com>
2020-05-22 22:02:07 -0500

    Classes and interference

--- javascriptFiles/app.js
+++ javascriptFiles/app.js
@@ -1,9 +1,29 @@
 "use strict";
 var Department = /** @class */ (function () {
-    function Department(n) {
-        this.name = n;
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        // private id: string;
+        // private name: string;
+        this.employees = [];
+        // this.name = n;
     }
+    Department.prototype.describe = function () {
+        console.log("Department (" + this.id + "): " + this.name);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
-var accounting = new Department('Accounting');
-console.log(accounting);
+var accounting = new Department('id', 'Accounting');
+accounting.addEmployee('Laycon');
+accounting.addEmployee('Stella');
+accounting.describe();
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: 'Laycon', describe: accounting.describe };
+// accountingCopy.describe();
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,36 @@
 class Department {
-name: string;
+    // private id: string;
+    // private name: string;
+    private employees: string[] = [];
 
-    constructor(n: string) {
+    constructor(private readonly id: string, public name: string) {
+        // this.name = n;
+    }
 
-        this.name = n;
+    describe(this: Department) {
+        console.log(`Department (${this.id}): ${this.name}`)
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees)
     }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department('id', 'Accounting');
 
-console.log(accounting)
+accounting.addEmployee('Laycon');
+accounting.addEmployee('Stella');
+
+
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: 'Laycon', describe: accounting.describe };
+
+// accountingCopy.describe();
