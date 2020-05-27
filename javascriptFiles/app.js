695f444e00347e344f2453182477f2c04c7d3705
LMTrain <layconmm@msn.com>
2020-05-27 14:54:38 -0500

    Type Guards

--- javascriptFiles/app.js
+++ javascriptFiles/app.js
@@ -1,10 +1,51 @@
 "use strict";
-var user1;
-user1 = {
+var e1 = {
     name: 'Max',
-    age: 30,
-    greet: function (phrase) {
-        console.log(phrase + ' ' + this.name);
-    }
+    privileges: ['create-server'],
+    startDate: new Date()
 };
-user1.greet('Hi there - I am');
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Privileges: ' + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log('Driving...');
+    };
+    return Car;
+}());
+var Truck = /** @class */ (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log('Driving a truck...');
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log('Loading Cargo...' + amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,76 @@
-interface Person {
+type Admin = {
     name: string;
-    age: number;
-
-    greet(phrase: string): void;
-
-}
-
-let user1: Person;
-
-user1 = {
-    name: 'Max',
-    age: 30,
-    greet(phrase: string) {
-        console.log(phrase + ' ' + this.name)
-    }
+    privileges: string[];
 };
 
-user1.greet('Hi there - I am');
+type Employee = {
+    name: string;
+    startDate: Date;
+};
+
+// interface ElevatedEmployee extends Employee, Admin {}
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date()
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+
+function add(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+type UnkownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnkownEmployee) {
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Privileges: ' + emp.startDate);
+    }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+    drive() {
+        console.log('Driving...')
+    }    
+}
+
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+
+    loadCargo(amount: number) {
+        console.log ('Loading Cargo...' + amount)
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
