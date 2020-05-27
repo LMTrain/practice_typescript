82be0b08f12bcfaeb8399f09c30118ebfc628fb9
LMTrain <layconmm@msn.com>
2020-05-27 15:32:51 -0500

    Index Properties

--- javascriptFiles/app.js
+++ javascriptFiles/app.js
@@ -65,3 +65,7 @@ var paragraph = document.querySelector('message-output');
 // const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
 var userInputElement = document.getElementById('user-input');
 userInputElement.value = 'Hi there';
+var errorBag = {
+    email: 'Not a valid email',
+    username: 'Must start with a capital character"'
+};
--- src/app.ts
+++ src/app.ts
@@ -107,3 +107,12 @@ const paragraph = document.querySelector('message-output');
 const userInputElement = document.getElementById('user-input')! as HTMLInputElement;
 
 userInputElement.value = 'Hi there';
+
+interface ErrorContainer { // { email: 'Not a valid email', username: 'Must start with Character}
+    [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+    email: 'Not a valid email',
+    username: 'Must start with a capital character"'
+};
