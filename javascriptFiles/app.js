6e693c8d96b5143872cd5950ba976ed2522e03c9
LMTrain <layconmm@msn.com>
2020-05-27 16:18:06 -0500

    Built in Generics Types

---
+++ javascriptFiles/app.js
@@ -0,0 +1,11 @@
+"use strict";
+var names = [];
+names[0].split(' ');
+var promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve('This is done!');
+    }, 2000);
+});
+promise.then(function (data) {
+    data.split(' ');
+});
