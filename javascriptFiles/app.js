ef0e91cfd2afc3c4f2768b1142e0dd1ce96ff36f
LMTrain <layconmm@msn.com>
2020-05-27 16:26:41 -0500

    Creating a Generic Function

--- javascriptFiles/app.js
+++ javascriptFiles/app.js
@@ -1,11 +1,16 @@
 "use strict";
-var names = [];
-names[0].split(' ');
-var promise = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        resolve('This is done!');
-    }, 2000);
-});
-promise.then(function (data) {
-    data.split(' ');
-});
+// const names: Array<string> = [];
+// names[0].split(' ');
+// const promise: Promise<string> | = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('This is done!');
+//     }, 2000);
+// });
+// promise.then(data => {
+//     data.split(' ');
+// })
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+var mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergeObj.age);
