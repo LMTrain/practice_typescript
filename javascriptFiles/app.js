46fc21cdf0e09b80afab90a0ea511f7db66d7338
LMTrain <layconmm@msn.com>
2020-06-01 21:37:32 -0500

    Another generic Function

--- javascriptFiles/app.js
+++ javascriptFiles/app.js
@@ -14,3 +14,14 @@ function merge(objA, objB) {
 }
 var mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 console.log(mergeObj.age);
+function countAndDescribe(element) {
+    var descriptionText = 'Got no value.';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element.';
+    }
+    else if (element.length > 1) {
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe('Hi there!'));
--- src/app.ts
+++ src/app.ts
@@ -17,3 +17,19 @@ function merge<T, U>(objA: T, objB: U) {
 
 const mergeObj = merge({name: 'Max', hobbies: ['Sports']}, {age: 30});
 console.log(mergeObj.age)
+
+interface length {
+    length: number;
+}
+
+function countAndDescribe<T extends length>(element: T) {
+    let descriptionText = 'Got no value.';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element.';
+    }else if (element.length > 1) {
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+    return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'))
