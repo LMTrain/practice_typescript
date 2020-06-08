31300eeba4e84567f88ff1b532639f59214f3e8f
LMTrain <layconmm@msn.com>
2020-06-07 22:51:22 -0500

    Adding Generics

--- javascriptFiles/app.js
+++ javascriptFiles/app.js
@@ -1,78 +1,12 @@
 "use strict";
-// const names: Array<string> = [];
-// names[0].split(' ');
-var __spreadArrays = (this && this.__spreadArrays) || function () {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-    for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
-    return r;
-};
-// const promise: Promise<string> | = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve('This is done!');
-//     }, 2000);
-// });
-// promise.then(data => {
-//     data.split(' ');
-// })
-function merge(objA, objB) {
-    return Object.assign(objA, objB);
+function Logger() {
+    console.log;
 }
-var mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergeObj.age);
-function countAndDescribe(element) {
-    var descriptionText = 'Got no value.';
-    if (element.length === 1) {
-        descriptionText = 'Got 1 element.';
+class Person {
+    constructor() {
+        this.name = 'Laycon';
+        console.log('Creating person object...');
     }
-    else if (element.length > 1) {
-        descriptionText = 'Got ' + element.length + ' elements.';
-    }
-    return [element, descriptionText];
 }
-console.log(countAndDescribe(['Sports', 'Cooking']));
-function extractAndConvert(obj, key) {
-    return 'Value: ' + obj[key];
-}
-console.log({ name: "Laycon" }, 'name');
-var DataStorage = /** @class */ (function () {
-    function DataStorage() {
-        this.data = [];
-    }
-    DataStorage.prototype.addItem = function (item) {
-        this.data.push(item);
-    };
-    DataStorage.prototype.removeItem = function (item) {
-        if (this.data.indexOf(item) === -1) {
-            return;
-        }
-        this.data.splice(this.data.indexOf(item), 1);
-    };
-    DataStorage.prototype.getItems = function () {
-        return __spreadArrays(this.data);
-    };
-    return DataStorage;
-}());
-var textStorage = new DataStorage();
-textStorage.addItem('Laycon');
-textStorage.addItem('Stella');
-textStorage.removeItem('Laycon');
-console.log(textStorage.getItems());
-var numberStotrage = new DataStorage();
-var objStorage = new DataStorage();
-var maxObj = { name: "Lisa" };
-objStorage.addItem({ maxObj: maxObj });
-objStorage.addItem({ name: "Lima" });
-// ...
-objStorage.removeItem(maxObj);
-console.log(objStorage.getItems());
-function createCourseGoal(title, description, date) {
-    var courseGoal = {};
-    courseGoal.title = title;
-    courseGoal.completeUntil = date;
-    return courseGoal;
-}
-var names = ['Max', 'Anna'];
-// names.push('Manu');
-// names.pop()
+const pers = new Person();
+console.log(pers);
--- src/app.ts
+++ src/app.ts
@@ -1,101 +1,15 @@
-// const names: Array<string> = [];
-// names[0].split(' ');
-
-// const promise: Promise<string> | = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve('This is done!');
-//     }, 2000);
-// });
-
-// promise.then(data => {
-//     data.split(' ');
-// })
-
-function merge<T, U>(objA: T, objB: U) {
-    return Object.assign(objA, objB);
+function Logger() {
+    console.log
 }
 
-const mergeObj = merge({name: 'Max', hobbies: ['Sports']}, {age: 30});
-console.log(mergeObj.age)
+class Person {
+    name = 'Laycon';
 
-interface lengthy {
-    length: number;
-}
-
-function countAndDescribe<T extends lengthy>(element: T): [T, string] {
-    let descriptionText = 'Got no value.';
-    if (element.length === 1) {
-        descriptionText = 'Got 1 element.';
-    }else if (element.length > 1) {
-        descriptionText = 'Got ' + element.length + ' elements.';
-    }
-    return [element, descriptionText];
-}
-
-console.log(countAndDescribe(['Sports', 'Cooking']));
-
-function extractAndConvert<T extends object, U extends keyof T>(
-    obj: T, 
-    key: U
-) {
-    return 'Value: ' + obj[key];
-}
-
-console.log({ name: "Laycon"}, 'name');
-
-class DataStorage<T extends string | number | boolean > {
-    private data: T[] = [];
-
-    addItem(item: T) {
-        this.data.push(item);
-    }
-
-    removeItem(item: T) {
-        if (this.data.indexOf(item) === -1) {
-            return;
-        }
-        this.data.splice(this.data.indexOf(item), 1);
-    }
-
-    getItems() {
-        return [...this.data];
+    constructor() {
+        console.log('Creating person object...');
     }
 }
 
-const textStorage = new DataStorage<string>();
-textStorage.addItem('Laycon');
-textStorage.addItem('Stella');
-textStorage.removeItem('Laycon');
-console.log(textStorage.getItems());
+const pers = new Person();
 
-const numberStotrage = new DataStorage<number>();
-
-const objStorage = new DataStorage<object>();
-
-const maxObj = {name: "Lisa"};
-objStorage.addItem({maxObj});
-objStorage.addItem({name: "Lima"});
-// ...
-objStorage.removeItem(maxObj);
-console.log(objStorage.getItems());
-
-interface CourseGoal {
-    title: string;
-    description: string;
-    completeUntil: Date;
-}
-
-function createCourseGoal(
-    title: string, 
-    description: string, 
-    date: Date
-): CourseGoal {
-    let courseGoal: Partial<CourseGoal> = {};
-    courseGoal.title = title;
-    courseGoal.completeUntil = date;
-    return courseGoal as CourseGoal;
-}
-
-const names: Readonly<string[]> = ['Max', 'Anna'];
-// names.push('Manu');
-// names.pop()
+console.log(pers);
